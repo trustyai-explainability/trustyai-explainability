c37a3328eccd1c1867bb6391bb17ccf9a5d365d0
Rui Vieira <ruidevieira@googlemail.com>
2022-07-07 10:32:41 +0100

    Add missing header

--- explainability-core/src/test/java/org/kie/trustyai/explainability/local/counterfactual/CounterfactualUtils.java
+++ explainability-core/src/test/java/org/kie/trustyai/explainability/local/counterfactual/CounterfactualUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kie.trustyai.explainability.local.counterfactual;
 
 import java.util.List;
@@ -24,7 +39,6 @@ public class CounterfactualUtils {
     public static final TimeUnit predictionTimeUnit = TimeUnit.MINUTES;
     public static final Long DEFAULT_STEPS = 30_000L;
     public static final double DEFAULT_GOAL_THRESHOLD = 0.01;
-
 
     public static CounterfactualResult runCounterfactualSearch(Long randomSeed, List<Output> goal,
             List<Feature> features,
