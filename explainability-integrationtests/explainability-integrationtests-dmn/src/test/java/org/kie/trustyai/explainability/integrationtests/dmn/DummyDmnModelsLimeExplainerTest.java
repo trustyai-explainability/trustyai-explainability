bf4e9fc692947208da3608b9c8de6d4524543ec9
Rui Vieira <ruivieira@users.noreply.github.com>
2022-11-15 21:04:45 +0000

    FAI-862: Update Kogito and OptaPlanner versions (#38)
    * Update POM
    
    * Disable dummy LIME tests
    
    * Add Jackson dependency to Arrow
    
    * Re-enable Dummy model LIME tests
    
    Fix Kogito formatter version.
    
    * Update Kogito formatter version
    
    * Fix formatting

--- explainability-integrationtests/explainability-integrationtests-dmn/src/test/java/org/kie/trustyai/explainability/integrationtests/dmn/DummyDmnModelsLimeExplainerTest.java
+++ explainability-integrationtests/explainability-integrationtests-dmn/src/test/java/org/kie/trustyai/explainability/integrationtests/dmn/DummyDmnModelsLimeExplainerTest.java
@@ -16,12 +16,7 @@
 package org.kie.trustyai.explainability.integrationtests.dmn;
 
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -33,18 +28,7 @@ import org.kie.kogito.dmn.DmnDecisionModel;
 import org.kie.trustyai.explainability.Config;
 import org.kie.trustyai.explainability.local.lime.LimeConfig;
 import org.kie.trustyai.explainability.local.lime.LimeExplainer;
-import org.kie.trustyai.explainability.model.DataDistribution;
-import org.kie.trustyai.explainability.model.Feature;
-import org.kie.trustyai.explainability.model.FeatureFactory;
-import org.kie.trustyai.explainability.model.FeatureImportance;
-import org.kie.trustyai.explainability.model.PerturbationContext;
-import org.kie.trustyai.explainability.model.Prediction;
-import org.kie.trustyai.explainability.model.PredictionInput;
-import org.kie.trustyai.explainability.model.PredictionInputsDataDistribution;
-import org.kie.trustyai.explainability.model.PredictionOutput;
-import org.kie.trustyai.explainability.model.PredictionProvider;
-import org.kie.trustyai.explainability.model.Saliency;
-import org.kie.trustyai.explainability.model.SimplePrediction;
+import org.kie.trustyai.explainability.model.*;
 import org.kie.trustyai.explainability.utils.DataUtils;
 import org.kie.trustyai.explainability.utils.ExplainabilityMetrics;
 import org.kie.trustyai.explainability.utils.ValidationUtils;
