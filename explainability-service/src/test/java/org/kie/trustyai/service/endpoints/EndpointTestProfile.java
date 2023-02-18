6ebafce53342164cad138fc3fd205c3a81f449b4
Rui Vieira <ruidevieira@googlemail.com>
2023-02-18 15:06:03 +0000

    Disable MinIO dev services

---
+++ explainability-service/src/test/java/org/kie/trustyai/service/endpoints/EndpointTestProfile.java
@@ -0,0 +1,26 @@
+package org.kie.trustyai.service.endpoints.metrics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+
+public class MetricsEndpointTestProfile implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+        final Map<String, String> overrides = new HashMap<>();
+        overrides.put("service.storage-format", "PVC");
+        overrides.put("service.model-name", "example");
+        overrides.put("service.data-format", "CSV");
+        overrides.put("service.metrics-schedule", "5s");
+        return overrides;
+    }
+
+    @Override
+    public Set<Class<?>> getEnabledAlternatives() {
+        return Set.of(MockMetricsDatasource.class);
+
+    }
+
+}
