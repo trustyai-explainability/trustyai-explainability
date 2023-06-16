b0c9d21bf23a0c2556f1e658a2413d8e655b95f6
Rob Geada <rob@geada.net>
2023-06-16 16:26:43 +0100

    Migrate biased model tests to CI

--- tests/basictests/trustyai_biased_models.sh
+++ tests/basictests/trustyai_biased_models.sh
@@ -12,6 +12,8 @@ TEST_PASS=${OPENSHIFT_TESTUSER_PASS:-"admin"} #Password used to login to the ODH
 OPENSHIFT_OAUTH_ENDPOINT="https://$(oc get route -n openshift-authentication   oauth-openshift -o json | jq -r '.spec.host')"
 MM_NAMESPACE="${ODHPROJECT}-model"
 
+MODEL_ALPHA=demo-loan-nn-onnx-alpha
+MODEL_BETA=demo-loan-nn-onnx-beta
 
 os::test::junit::declare_suite_start "$MY_SCRIPT"
 
@@ -28,26 +30,25 @@ function check_trustyai_resources() {
   os::cmd::try_until_text "oc get deployment trustyai-service" "trustyai-service" $odhdefaulttimeout $odhdefaultinterval
   os::cmd::try_until_text "oc get route trustyai-service-route" "trustyai-service-route" $odhdefaulttimeout $odhdefaultinterval
 
-  oc wait --for=condition=Ready $(oc get pod -o name | grep trustyai) --timeout=${odhdefaulttimeout}ms
 
+
+  oc wait --for=condition=Ready $(oc get pod -o name | grep trustyai) --timeout=${odhdefaulttimeout}ms
 }
 
 function deploy_model() {
-    header "Deploying model into ModelMesh"
-    oc new-project $MM_NAMESPACE  || true
+    header "Deploying models into ModelMesh"
+    oc new-project $MM_NAMESPACE || true
     os::cmd::expect_success "oc project $MM_NAMESPACE"
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/modelmesh/service_account.yaml -n ${MM_NAMESPACE}"
     oc label namespace $MM_NAMESPACE "modelmesh-enabled=true" --overwrite=true || echo "Failed to apply modelmesh-enabled label."
     os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/secret.yaml -n ${MM_NAMESPACE}"
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/odh-mlserver-0.x.yaml  -n ${MM_NAMESPACE}"
-#    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/default_sklearn_model.yaml  -n ${MM_NAMESPACE}"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/ovms-1.x.yaml  -n ${MM_NAMESPACE}"
 
     SECRETKEY=$(openssl rand -hex 32)
     sed -i "s/<secretkey>/$SECRETKEY/g" ${RESOURCEDIR}/trustyai/sample-minio.yaml
     os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/sample-minio.yaml -n ${MM_NAMESPACE}"
-    #os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/openvino-serving-runtime.yaml -n ${MM_NAMESPACE}"
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/models/openvino-inference-service.yaml -n ${MM_NAMESPACE}"
-    sleep 30
+
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/models/model0_onnx.yaml  -n ${MM_NAMESPACE}"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/models/model1_onnx.yaml  -n ${MM_NAMESPACE}"
 
 
 }
@@ -55,35 +56,34 @@ function deploy_model() {
 function check_mm_resources() {
   header "Checking that ModelMesh resources have spun up"
   oc project $MM_NAMESPACE
-  os::cmd::try_until_text "oc get route example-sklearn-isvc" "example-sklearn-isvc" $odhdefaulttimeout $odhdefaultinterval
-  INFER_ROUTE=$(oc get route example-sklearn-isvc --template={{.spec.host}}{{.spec.path}})
-  token=$(oc create token user-one -n ${MM_NAMESPACE})
+  os::cmd::try_until_text "oc get route demo-loan-nn-onnx-alpha" "demo-loan-nn-onnx-alpha" $odhdefaulttimeout $odhdefaultinterval
+  os::cmd::try_until_text "oc get route demo-loan-nn-onnx-beta" "demo-loan-nn-onnx-beta" $odhdefaulttimeout $odhdefaultinterval
+
   os::cmd::try_until_text "oc get pod | grep modelmesh-serving" "5/5" $odhdefaulttimeout $odhdefaultinterval
-  os::cmd::try_until_text "curl -k https://$INFER_ROUTE/infer -d @${RESOURCEDIR}/trustyai/data.json -H 'Authorization: Bearer $token' -i" "model_name"
+
+  INFER_ROUTE_ALPHA=$(oc get route demo-loan-nn-onnx-alpha --template={{.spec.host}}{{.spec.path}})
+  INFER_ROUTE_BETA=$(oc get route demo-loan-nn-onnx-alpha --template={{.spec.host}}{{.spec.path}})
+  os::cmd::try_until_text "curl -k https://$INFER_ROUTE_ALPHA/infer -d @${RESOURCEDIR}/data/loan_default/dummy_data.json" "model_name"
+  os::cmd::try_until_text "curl -k https://$INFER_ROUTE_BETA/infer -d @${RESOURCEDIR}/data/loan_default/dummy_data.json" "model_name"
+  echo "Waiting 10s for endpoints to stabilize"
+  sleep 10
 }
+
 
 function check_communication(){
     header "Check communication between TrustyAI and ModelMesh"
     oc project $MM_NAMESPACE
 
     # send some data to modelmesh
-    os::cmd::expect_success_and_text "curl -k https://$INFER_ROUTE/infer -d @${RESOURCEDIR}/trustyai/data.json -H 'Authorization: Bearer $token' -i" "model_name"
     oc project ${ODHPROJECT}
     os::cmd::try_until_text "oc logs $(oc get pods -o name | grep trustyai-service)" "Received partial input payload" $odhdefaulttimeout $odhdefaultinterval
 }
 
-function generate_data(){
-    header "Generate some data for TrustyAI (this will take a sec)"
+function send_data(){
+    header "Sending some data for TrustyAI (this will take a minute or two)"
     oc project $MM_NAMESPACE
 
-    # send a bunch of random data to the model
-    DIVISOR=128.498 # divide bash's $RANDOM by this to get a float range of [0.,255.], for MNIST
-    for i in {1..500};
-    do
-      DATA=$(sed "s/\[40.83, 3.5, 0.5, 0\]/\[$(($RANDOM % 2)),$(($RANDOM / 128)),$(($RANDOM / 128)), $(($RANDOM / 128)) \]/" ${RESOURCEDIR}/trustyai/data.json)
-      curl -k https://$INFER_ROUTE/infer -d "$DATA"  -H 'Authorization: Bearer $token' -i > /dev/null 2>&1 &
-      sleep .01
-    done
+    $RESOURCEDIR/utils/send_data_batch $RESOURCEDIR/data/loan_default/batch_01.json
 }
 
 function schedule_and_check_request(){
@@ -91,17 +91,52 @@ function schedule_and_check_request(){
   oc project $ODHPROJECT
   TRUSTY_ROUTE=$(oc get route/trustyai --template={{.spec.host}})
 
-  os::cmd::expect_success_and_text "curl --location http://$TRUSTY_ROUTE/metrics/spd/request \
-    --header 'Content-Type: application/json' \
-    --data '{
-        \"modelId\": \"example-sklearn-isvc\",
-        \"protectedAttribute\": \"input-0\",
-        \"favorableOutcome\": 0,
-        \"outcomeName\": \"output-0\",
-        \"privilegedAttribute\": 0.0,
-        \"unprivilegedAttribute\": 1.0
-    }'" "requestId"
-  os::cmd::try_until_text "curl http://$TRUSTY_ROUTE/q/metrics" "trustyai_spd"
+  for METRIC_NAME in "spd" "dir"
+  do
+    METRIC_UPPERCASE=$(echo ${METRIC_NAME} | tr '[:lower:]' '[:upper:]')
+    for MODEL in $MODEL_ALPHA $MODEL_BETA
+    do
+      curl -s --location http://$TRUSTY_ROUTE/metrics/$METRIC_NAME/request \
+        --header 'Content-Type: application/json' \
+        --data "{
+                  \"modelId\": \"$MODEL\",
+                  \"protectedAttribute\": \"input-3\",
+                  \"favorableOutcome\":  0,
+                  \"outcomeName\": \"output-0\",
+                  \"privilegedAttribute\": 1.0,
+                  \"unprivilegedAttribute\": 0.0
+                }"
+      echo ": Registered real-time monitoring of $METRIC_UPPERCASE"
+    done
+  done
+}
+
+function send_more_data(){
+    header "Sending some data for TrustyAI (this will take a minute or two)"
+    oc project $MM_NAMESPACE
+
+  LOOP_IDX=1
+  for batch in $(ls $RESOURCEDIR/data/loan_default/)
+  do
+      if [ "$batch" = "training_data.json" ]; then
+        :
+      elif [ "$batch" = "batch_01.json" ]; then
+        :
+      elif [ "$batch" = "dummy_data.json" ]; then
+         :
+      else
+        echo "===== Deployment Day $LOOP_IDX ====="
+        $RESOURCEDIR/utils/send_data_batch $batch
+
+        for i in {1..5}
+        do
+          echo -ne "\rSleeping for rest of day...$((5 - $i))"
+          sleep 1
+        done
+        echo
+      fi
+      let "LOOP_IDX++"
+    done
 }
 
 
@@ -109,11 +144,15 @@ function test_prometheus_scraping(){
     header "Ensure metrics are in Prometheus"
     MODEL_MONITORING_ROUTE=$(oc get route -n ${ODHPROJECT} odh-model-monitoring --template={{.spec.host}})
     os::cmd::try_until_text "curl -k --location -g --request GET 'https://'$MODEL_MONITORING_ROUTE'//api/v1/query?query=trustyai_spd' -H 'Authorization: Bearer $TESTUSER_BEARER_TOKEN' -i" "value" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::try_until_text "curl -k --location -g --request GET 'https://'$MODEL_MONITORING_ROUTE'//api/v1/query?query=trustyai_dir' -H 'Authorization: Bearer $TESTUSER_BEARER_TOKEN' -i" "value" $odhdefaulttimeout $odhdefaultinterval
 }
+
 
 function teardown_trustyai_test() {
   header "Cleaning up the TrustyAI test"
   oc project $ODHPROJECT
+
+  REQUEST_ID="$(curl http://$TRUSTY_ROUTE/metrics/spd/requests | jq '.requests [0].id')"
 
   # delete all requests
   for METRIC_NAME in "spd" "dir"
@@ -132,20 +171,22 @@ function teardown_trustyai_test() {
 
   oc project $MM_NAMESPACE
   os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/secret.yaml"
-  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/odh-mlserver-0.x.yaml"
-  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/models/default_sklearn_model.yaml"
+  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/ovms-1.x.yaml"
+  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/models/model0_onnx.yaml"
+  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/models/model1_onnx.yaml"
   os::cmd::expect_success "oc delete project $MM_NAMESPACE"
-
 }
 
+
+
 get_authentication
+check_trustyai_resources
 deploy_model
 check_mm_resources
 check_communication
-generate_data
+send_data
 schedule_and_check_request
 test_prometheus_scraping
 teardown_trustyai_test
-
 
 os::test::junit::declare_suite_end
