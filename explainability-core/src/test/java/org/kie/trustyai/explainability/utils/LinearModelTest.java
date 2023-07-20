a7092db4836a8227576f303b4c63989b581b7fc9
Tommaso Teofili <tteofili@redhat.com>
2023-07-20 14:56:24 +0200

    fixed seeds for LinearModelTest

--- explainability-core/src/test/java/org/kie/trustyai/explainability/utils/LinearModelTest.java
+++ explainability-core/src/test/java/org/kie/trustyai/explainability/utils/LinearModelTest.java
@@ -23,7 +23,6 @@ import java.util.stream.DoubleStream;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
