967feddc39c3f5ce8e7c975e5264667c7e01c388
Rob Geada <rob@geada.net>
2023-08-08 12:52:29 +0100

    Make individual metrics respect batch size (#315)

--- explainability-service/src/main/java/org/kie/trustyai/service/endpoints/metrics/fairness/group/GroupEndpoint.java
+++ explainability-service/src/main/java/org/kie/trustyai/service/endpoints/metrics/fairness/group/GroupEndpoint.java
@@ -1,5 +1,7 @@
 package org.kie.trustyai.service.endpoints.metrics.fairness.group;
 
+import java.util.Objects;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -53,7 +55,12 @@ public abstract class GroupEndpoint extends BaseEndpoint<GroupMetricRequest> {
         final Dataframe dataframe;
         final Metadata metadata;
         try {
-            dataframe = super.dataSource.get().getDataframe(request.getModelId()).filterRowsBySynthetic(false);
+            if (Objects.isNull(request.getBatchSize())) {
+                final int defaultBatchSize = serviceConfig.batchSize().getAsInt();
+                LOG.warn("Request batch size is empty. Using the default value of " + defaultBatchSize);
+                request.setBatchSize(defaultBatchSize);
+            }
+            dataframe = super.dataSource.get().getDataframe(request.getModelId(), request.getBatchSize()).filterRowsBySynthetic(false);
             metadata = dataSource.get().getMetadata(request.getModelId());
         } catch (DataframeCreateException e) {
             LOG.error("No data available for model " + request.getModelId() + ": " + e.getMessage(), e);
