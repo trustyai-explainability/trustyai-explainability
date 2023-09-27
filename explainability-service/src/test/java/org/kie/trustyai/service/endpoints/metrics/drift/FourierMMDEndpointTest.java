cd1ebf8baa6d474246e99c590daec00fa72e6928
Jim Rayfield <jtray@us.ibm.com>
2023-09-27 17:15:32 -0400

    edit populateStorage() to read train and valid CSV files, and to generate Dataframe from those.

--- explainability-service/src/test/java/org/kie/trustyai/service/endpoints/metrics/drift/FourierMMDEndpointTest.java
+++ explainability-service/src/test/java/org/kie/trustyai/service/endpoints/metrics/drift/FourierMMDEndpointTest.java
@@ -1,5 +1,11 @@
 package org.kie.trustyai.service.endpoints.metrics.drift;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,6 +17,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kie.trustyai.explainability.model.Dataframe;
+import org.kie.trustyai.explainability.model.Feature;
+import org.kie.trustyai.explainability.model.Output;
+import org.kie.trustyai.explainability.model.Prediction;
+import org.kie.trustyai.explainability.model.PredictionInput;
+import org.kie.trustyai.explainability.model.PredictionOutput;
+import org.kie.trustyai.explainability.model.SimplePrediction;
+import org.kie.trustyai.explainability.model.Type;
+import org.kie.trustyai.explainability.model.Value;
 import org.kie.trustyai.metrics.drift.fouriermmd.FourierMMD;
 import org.kie.trustyai.metrics.drift.fouriermmd.FourierMMDFitting;
 import org.kie.trustyai.service.endpoints.metrics.MetricsEndpointTestProfile;
@@ -47,15 +61,81 @@ class FourierMMDEndpointTest {
     @Inject
     Instance<MockPrometheusScheduler> scheduler;
 
+    final public static String trainDataSetFileName = "train_ts_x.csv";
+    final public static String validDataSetFileName = "valid_ts_x.csv";
+    final public static String testDataSetFileName = "test_ts_x.csv";
+
+    public Dataframe readCSV(String fileName) {
+
+        BufferedReader br = null;
+        List<Prediction> predictions = null;
+        try {
+            final InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
+            final InputStreamReader isr = new InputStreamReader(is);
+            br = new BufferedReader(isr);
+
+            br.readLine(); // skip header line
+
+            predictions = new ArrayList<Prediction>();
+
+            while (true) {
+
+                final String line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+
+                String[] values = line.split(",");
+                assert values.length == 13;
+
+                final Feature x1 = new Feature("X1", Type.NUMBER, new Value(Double.parseDouble(values[2])));
+                final Feature x2 = new Feature("X2", Type.NUMBER, new Value(Double.parseDouble(values[3])));
+                final Feature x3 = new Feature("X3", Type.NUMBER, new Value(Double.parseDouble(values[4])));
+                final Feature x4 = new Feature("X4", Type.NUMBER, new Value(Double.parseDouble(values[5])));
+                final Feature x5 = new Feature("X5", Type.NUMBER, new Value(Double.parseDouble(values[6])));
+                final Feature x6 = new Feature("X6", Type.NUMBER, new Value(Double.parseDouble(values[7])));
+                final Feature x7 = new Feature("X7", Type.NUMBER, new Value(Double.parseDouble(values[8])));
+                final Feature x8 = new Feature("X8", Type.NUMBER, new Value(Double.parseDouble(values[9])));
+                final Feature x9 = new Feature("X9", Type.NUMBER, new Value(Double.parseDouble(values[10])));
+                final Feature x10 = new Feature("X10", Type.NUMBER, new Value(Double.parseDouble(values[11])));
+
+                final List<Feature> features = new ArrayList<Feature>(
+                        Arrays.asList(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10));
+
+                final PredictionInput predIn = new PredictionInput(features);
+                final PredictionOutput predOut = new PredictionOutput(new ArrayList<Output>());
+                final Prediction prediction = new SimplePrediction(predIn, predOut);
+
+                predictions.add(prediction);
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("unexpected Exception", e);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    //
+                }
+            }
+        }
+
+        return Dataframe.createFrom(predictions);
+    }
+
     @BeforeEach
     void populateStorage() throws JsonProcessingException {
         storage.get().emptyStorage();
-        Dataframe dataframe = datasource.get().generateRandomDataframe(N_SAMPLES);
+
+        Dataframe dataframe = readCSV(trainDataSetFileName);
 
         HashMap<String, List<List<Integer>>> tagging = new HashMap<>();
         tagging.put(TRAINING_TAG, List.of(List.of(0, N_SAMPLES)));
         dataframe.tagDataPoints(tagging);
-        dataframe.addPredictions(datasource.get().generateRandomDataframeDrifted(N_SAMPLES).asPredictions());
+
+        Dataframe validDF = readCSV(validDataSetFileName);
+        dataframe.addPredictions(validDF.asPredictions());
+
         datasource.get().saveDataframe(dataframe, MODEL_ID);
         datasource.get().saveMetadata(datasource.get().createMetadata(dataframe), MODEL_ID);
     }
