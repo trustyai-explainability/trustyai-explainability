ce17a042f79e31daf14cc8c62673cca008dceaf1
Rui Vieira <ruidevieira@googlemail.com>
2023-11-20 12:25:08 +0000

    Fix formatting

--- explainability-core/src/test/java/org/kie/trustyai/validation/explainability/metrics/FairnessAIFValidationTest.java
+++ explainability-core/src/test/java/org/kie/trustyai/validation/explainability/metrics/FairnessAIFValidationTest.java
@@ -1,12 +1,5 @@
 package org.kie.trustyai.validation.explainability.metrics;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.kie.trustyai.explainability.model.*;
-import org.kie.trustyai.metrics.fairness.group.DisparateImpactRatio;
-import org.kie.trustyai.metrics.fairness.group.GroupStatisticalParityDifference;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,6 +8,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.kie.trustyai.explainability.model.*;
+import org.kie.trustyai.metrics.fairness.group.DisparateImpactRatio;
+import org.kie.trustyai.metrics.fairness.group.GroupStatisticalParityDifference;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,7 +26,7 @@ public class FairnessAIFValidationTest {
     public void setUp() {
         final ClassLoader classLoader = getClass().getClassLoader();
         try (InputStream inputStream = classLoader.getResourceAsStream("validation/extdata/data.csv");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
 
             // Skip the header
             List<String> lines = reader.lines().skip(1).collect(Collectors.toList());
@@ -39,15 +39,15 @@ public class FairnessAIFValidationTest {
                 final Feature fnlwgt = FeatureFactory.newNumericalFeature("fnlwgt", Integer.parseInt(values[2]));
                 final Feature education = FeatureFactory.newNumericalFeature("education", Integer.parseInt(values[3]));
                 final Feature education_num = FeatureFactory.newNumericalFeature("education-num", Integer.parseInt(values[4]));
-                final Feature marital_status= FeatureFactory.newNumericalFeature("marital-status", Integer.parseInt(values[5]));
+                final Feature marital_status = FeatureFactory.newNumericalFeature("marital-status", Integer.parseInt(values[5]));
                 final Feature occupation = FeatureFactory.newNumericalFeature("occupation", Integer.parseInt(values[6]));
                 final Feature relationship = FeatureFactory.newNumericalFeature("relationship", Integer.parseInt(values[7]));
                 final Feature race = FeatureFactory.newNumericalFeature("race", Integer.parseInt(values[8]));
-                final Feature sex= FeatureFactory.newNumericalFeature("sex", Integer.parseInt(values[9]));
+                final Feature sex = FeatureFactory.newNumericalFeature("sex", Integer.parseInt(values[9]));
                 final Feature capital_gain = FeatureFactory.newNumericalFeature("capital-gain", Integer.parseInt(values[10]));
-                final Feature capital_loss= FeatureFactory.newNumericalFeature("capital-loss", Integer.parseInt(values[11]));
+                final Feature capital_loss = FeatureFactory.newNumericalFeature("capital-loss", Integer.parseInt(values[11]));
                 final Feature hours_per_week = FeatureFactory.newNumericalFeature("hours-per-week", Integer.parseInt(values[12]));
-                final Feature native_country= FeatureFactory.newNumericalFeature("native-country", Integer.parseInt(values[13]));
+                final Feature native_country = FeatureFactory.newNumericalFeature("native-country", Integer.parseInt(values[13]));
                 final Output income = new Output("income", Type.NUMBER, new Value(Integer.parseInt(values[14])), 1d);
 
                 final PredictionInput input = new PredictionInput(List.of(age, workclass, fnlwgt, education, education_num,
