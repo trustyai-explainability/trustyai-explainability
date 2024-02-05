ddac56d4f6d206cdb35c40252c61dbca8801f876
Rob Geada <rob@geada.net>
2024-02-05 11:43:18 +0000

    Fix single-valued metrics being wrapped into arrays in request listing endpoints

--- explainability-service/src/main/java/org/kie/trustyai/service/payloads/values/reconcilable/ReconcilableField.java
+++ explainability-service/src/main/java/org/kie/trustyai/service/payloads/values/reconcilable/ReconcilableField.java
@@ -3,26 +3,48 @@ package org.kie.trustyai.service.payloads.values.reconcilable;
 import java.util.List;
 import java.util.Optional;
 
+import org.jboss.logging.Logger;
 import org.kie.trustyai.service.payloads.values.TypedValue;
 
 import com.fasterxml.jackson.databind.node.ValueNode;
 
 public abstract class ReconcilableField {
+    private static final Logger LOG = Logger.getLogger(ReconcilableField.class);
+
     private final List<ValueNode> rawValueNodes;
+    private final ValueNode rawValueNode;
     private Optional<List<TypedValue>> reconciledType;
 
     protected ReconcilableField(ValueNode rawValueNode) {
-        this.rawValueNodes = List.of(rawValueNode);
+        this.rawValueNode = rawValueNode;
+        this.rawValueNodes = null;
         this.reconciledType = Optional.empty();
     }
 
     protected ReconcilableField(List<ValueNode> rawValueNode) {
         this.rawValueNodes = rawValueNode;
+        this.rawValueNode = null;
         this.reconciledType = Optional.empty();
     }
 
     public List<ValueNode> getRawValueNodes() {
-        return rawValueNodes;
+        if (isMultipleValued()) {
+            return rawValueNodes;
+        } else {
+            return List.of(rawValueNode);
+        }
+    }
+
+    public ValueNode getRawValueNode() {
+        if (!isMultipleValued()) {
+            return rawValueNode;
+        } else {
+            throw new IllegalArgumentException("Cannot return single value of multiple-valued ReconcilableField");
+        }
+    }
+
+    public boolean isMultipleValued() {
+        return rawValueNodes != null;
     }
 
     public Optional<List<TypedValue>> getReconciledType() {
@@ -34,6 +56,10 @@ public abstract class ReconcilableField {
     }
 
     public String toString() {
-        return this.getRawValueNodes().toString();
+        if (isMultipleValued()) {
+            return rawValueNodes.toString();
+        } else {
+            return rawValueNode.toString();
+        }
     }
 }
