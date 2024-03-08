59309d32b1f5ced497a83ae94f541472e86417ce
Rob Geada <rob@geada.net>
2024-03-08 09:24:58 +0000

    Add migrator class

---
+++ explainability-service/src/main/java/org/kie/trustyai/service/data/storage/hibernate/HibernateMigrator.java
@@ -0,0 +1,11 @@
+package org.kie.trustyai.service.data.storage.hibernate;
+
+import java.util.List;
+
+public class HibernateMigrator {
+    List<String> csvModels;
+
+    public HibernateMigrator(){
+
+    }
+}
