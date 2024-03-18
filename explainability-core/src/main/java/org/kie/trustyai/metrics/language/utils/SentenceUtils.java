1dbdb1156859c38a311bb0e71aa3374d7e94d689
Christina Xu <77992688+christinaexyou@users.noreply.github.com>
2024-03-18 08:23:31 -0400

    add rouge metric (#526)

---
+++ explainability-core/src/main/java/org/kie/trustyai/metrics/language/utils/SentenceUtils.java
@@ -0,0 +1,23 @@
+package org.kie.trustyai.metrics.language.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+
+public class SentenceUtils {
+    private SentenceUtils() {
+    }
+
+    public static String[] splitSentences(String text){
+          try (InputStream modelIn = new FileInputStream("/opennlp/models/en-token.bin")) {
+            SentenceDetectorME detector = new SentenceDetectorME(new SentenceModel(modelIn));
+            return detector.sentDetect(text);
+        } catch (IOException e) {
+            String[] res = text.split("[.,!?:;]+\\s*");
+            return res;
+        }
+    }
+}
