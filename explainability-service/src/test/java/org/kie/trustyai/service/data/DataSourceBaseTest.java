280bbd67b6219d4a7db493ba895656eb8ea046eb
Rui Vieira <ruidevieira@googlemail.com>
2024-03-22 14:19:45 +0000

    Add additional test

--- explainability-service/src/test/java/org/kie/trustyai/service/data/DataSourceBaseTest.java
+++ explainability-service/src/test/java/org/kie/trustyai/service/data/DataSourceBaseTest.java
@@ -1,5 +1,6 @@
 package org.kie.trustyai.service.data;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.kie.trustyai.explainability.model.*;
 import org.kie.trustyai.service.mocks.MockDatasource;
@@ -36,6 +37,7 @@ abstract class DataSourceBaseTest {
     }
 
     @Test
+    @DisplayName("Ensure internals ids are batched when reading batch data")
     void testReadingBatch() {
         final int N = 10000;
         final Random random = new Random();
@@ -52,11 +54,48 @@ abstract class DataSourceBaseTest {
         }
         final Dataframe dataframe = Dataframe.createFrom(predictions);
         datasource.get().saveDataframe(dataframe, MODEL_ID);
-        final Dataframe readDataframe = datasource.get().getDataframe("fake-model", 20);
+
+        final int BATCH_SIZE = random.nextInt(1000) + 1;
+
+        final Dataframe readDataframe = datasource.get().getDataframe("fake-model", BATCH_SIZE);
         assertNotNull(readDataframe);
-        assertEquals(20, readDataframe.getRowDimension());
+
+
+        assertEquals(BATCH_SIZE, readDataframe.getRowDimension());
         final List<String> readIds = readDataframe.getIds();
-        final List<String> originalIds = ids.subList(ids.size() - 20, ids.size());
+        final List<String> originalIds = ids.subList(ids.size() - BATCH_SIZE, ids.size());
         assertEquals(originalIds, readIds);
     }
+
+    @Test
+    @DisplayName("Read with batch larger than the number of elements")
+    void testReadingBatchLarger() {
+        final int N = 100;
+        final Random random = new Random();
+        final List<String> ids = new ArrayList<>();
+        final List<Prediction> predictions = new ArrayList<>();
+        for (int i = 0 ; i < N ; i++) {
+            final PredictionInput input = new PredictionInput(List.of(FeatureFactory.newNumericalFeature("a", random.nextDouble())));
+            final PredictionOutput output = new PredictionOutput(List.of(new Output("b", Type.NUMBER, new Value(random.nextDouble()), 1.0)));
+            final String id = UUID.randomUUID().toString();
+            ids.add(id);
+            final PredictionMetadata metadata = new PredictionMetadata(id, LocalDateTime.now(), Dataframe.InternalTags.UNLABELED.get());
+            final Prediction prediction = new SimplePrediction(input, output, metadata);
+            predictions.add(prediction);
+        }
+        final Dataframe dataframe = Dataframe.createFrom(predictions);
+        datasource.get().saveDataframe(dataframe, MODEL_ID);
+
+        final int BATCH_SIZE = random.nextInt(1000) + 101;
+
+        final Dataframe readDataframe = datasource.get().getDataframe("fake-model", BATCH_SIZE);
+        assertNotNull(readDataframe);
+
+
+        assertEquals(N, readDataframe.getRowDimension());
+        final List<String> readIds = readDataframe.getIds();
+        final List<String> originalIds = ids.subList(ids.size() - N, ids.size());
+        assertEquals(originalIds, readIds);
+    }
+
 }
