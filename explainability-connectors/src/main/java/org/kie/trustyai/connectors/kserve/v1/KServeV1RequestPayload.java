0e81063675b4938532ece12b4a148f48cd4ac113
Rui Vieira <ruidevieira@googlemail.com>
2024-04-28 22:48:58 +0100

    fix: Add unique name to V1 HTTP features

--- explainability-connectors/src/main/java/org/kie/trustyai/connectors/kserve/v1/KServeV1RequestPayload.java
+++ explainability-connectors/src/main/java/org/kie/trustyai/connectors/kserve/v1/KServeV1RequestPayload.java
@@ -1,10 +1,15 @@
 package org.kie.trustyai.connectors.kserve.v1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import org.kie.trustyai.explainability.model.Feature;
 import org.kie.trustyai.explainability.model.FeatureFactory;
 import org.kie.trustyai.explainability.model.PredictionInput;
+
+import static org.kie.trustyai.connectors.kserve.PayloadParser.DEFAULT_INPUT_PREFIX;
 
 public class KServeV1RequestPayload {
 
@@ -19,8 +24,16 @@ public class KServeV1RequestPayload {
     }
 
     public List<PredictionInput> toPredictionInputs() {
-        return instances.stream().map(values -> values.stream().map(value -> FeatureFactory.newNumericalFeature("f", value))
-                .collect(Collectors.toList())).map(PredictionInput::new).collect(Collectors.toList());
+
+        final List<PredictionInput> predictionInputs = new ArrayList<>();
+        for (List<Double> instance : instances) {
+            final int size = instance.size();
+            final List<Feature> features = IntStream.range(0, size)
+                    .mapToObj(i -> FeatureFactory.newNumericalFeature(DEFAULT_INPUT_PREFIX + "-" + i, instance.get(i)))
+                    .collect(Collectors.toUnmodifiableList());
+            predictionInputs.add(new PredictionInput(features));
+        }
+        return predictionInputs;
     }
 
     @Override
