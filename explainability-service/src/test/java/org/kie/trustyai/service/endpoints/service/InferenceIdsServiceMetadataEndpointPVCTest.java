c09ed12101c5043be0141404258b7d7cbddf932b
Rui Vieira <ruidevieira@googlemail.com>
2024-06-25 15:26:04 +0100

    RHOAIENG-9007: Add inference ids metadata endpoint (#582)
    * feat: Add inference ids metadata endpoint
    
    * Rename PredictionId to InferenceId
    
    * Fix README.md
    
    * Fix endpoint summary

---
+++ explainability-service/src/test/java/org/kie/trustyai/service/endpoints/service/InferenceIdsServiceMetadataEndpointPVCTest.java
@@ -0,0 +1,27 @@
+package org.kie.trustyai.service.endpoints.service;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeEach;
+import org.kie.trustyai.service.mocks.MockPVCStorage;
+import org.kie.trustyai.service.profiles.PVCTestProfile;
+
+import java.io.IOException;
+
+@QuarkusTest
+@TestProfile(PVCTestProfile.class)
+public class InferenceIdsServiceMetadataEndpointPVCTest extends InferenceIdsServiceMetadataEndpointBaseTest {
+
+    @Inject
+    Instance<MockPVCStorage> storage;
+
+    @BeforeEach
+    void reset() throws IOException {
+        storage.get().emptyStorage("/tmp/" + MODEL_ID + "-data.csv");
+        storage.get().emptyStorage("/tmp/" + MODEL_ID + "-internal_data.csv");
+        storage.get().emptyStorage("/tmp/" + MODEL_ID + "-metadata.json");
+    }
+
+}
