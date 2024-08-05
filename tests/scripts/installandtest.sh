c6208a1ed0b3dfda38498340d08381e4d3642770
aaguirre <aaguirre@redhat.com>
2024-08-05 11:41:13 +0200

    Add marker to only run openshift tests

--- tests/scripts/installandtest.sh
+++ tests/scripts/installandtest.sh
@@ -56,7 +56,7 @@ success=1
 
 if [ $INSTALL_FAILURE = false ]; then
   cd peak/trustyai-tests
-  poetry run pytest -s --log-cli-level=DEBUG
+  poetry run pytest -s --log-cli-level=DEBUG -m openshift
 else
   echo -e "Skipping tests due to ODH Operator/DSC installation failure, marking suite as failed."
   success=0
