7ca0b0ceb9100f8af7829a0818d39a8b5e33efc2
aaguirre <aaguirre@redhat.com>
2024-08-13 13:06:56 +0200

    Add option to apply modelmesh configmap

--- tests/scripts/installandtest.sh
+++ tests/scripts/installandtest.sh
@@ -56,7 +56,7 @@ success=1
 
 if [ $INSTALL_FAILURE = false ]; then
   cd peak/trustyai-tests
-  poetry run pytest -s --log-cli-level=DEBUG -m openshift
+  poetry run pytest -s --log-cli-level=DEBUG -m openshift --modelmesh-configmap
 else
   echo -e "Skipping tests due to ODH Operator/DSC installation failure, marking suite as failed."
   success=0
