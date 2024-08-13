daae9b839289f1fe95cd5d6ea90321ee7f0783fc
Rob Geada <rob@geada.net>
2024-08-13 15:26:35 +0100

    Add 404 response filter

---
+++ explainability-service/src/test/java/org/kie/trustyai/service/endpoints/service/EndpointResponseFilterTest.java
@@ -0,0 +1,26 @@
+package org.kie.trustyai.service.endpoints.service;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import org.junit.jupiter.api.Test;
+import org.kie.trustyai.service.profiles.flatfile.PVCTestProfile;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+
+@QuarkusTest
+@TestProfile(PVCTestProfile.class)
+class EndpointResponseFilterTest {
+    
+    @Test
+    void test404() {
+        String endpoint = "/this/endpoint/does/not/exist/123456abcdef";
+        given()
+                .when().get(endpoint).peek()
+                .then()
+                .statusCode(404)
+                .body(is(String.format(EndpointResponseFilter.NOT_FOUND_MESSAGE_FMT, endpoint)));
+    }
+
+}
